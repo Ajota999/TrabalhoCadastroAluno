a1340906d748c247411c9b53ff7196a80da17b94
Ajota999 <gabrielfalavinhagomes@gmail.com>
2019-11-02 22:25:30 -0300

    fiz um menu temporario, criei uma funcao pra fazer a verificacao do nome, e coloquei algumas coisas em cadastrar() - obs.: nao estao funcionando as coisas mexo dps

--- Main.c
+++ Main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 //numero maximo de estudantes
 #define	MAX 20
-//alo
 //numero maximo de caracteres no nome
 #define NOME 50
 
@@ -28,7 +28,10 @@ struct Estudante{
 typedef struct Estudante est;
 
 //funcao de cadastro do aluno
-void cadastro(est A[], int pont);
+void cadastrar(est A[], int pont);
+
+//verificacao do nome
+char nomeVer(char* nome[]);
 
 //funcao de listar todos os alunos
 void listar(est A[], int pont);
@@ -42,25 +45,89 @@ void atualizar(est A[]);
 //funcao que invalida o cadastro do aluno
 void remover(est A[]);
 
+//funcao do menu do usuario (por enquanto temporario)
+char menu();
+
+//funcao de sair do programa
+void sair();
+
 int main(){
 	//ponteiro do ID
 	int idPont = 0;
-	
 	est aluno[MAX];
+	char escolha = menu();
+
+	do{
+		switch(escolha){
+			case '1':
+				if(idPont == MAX){
+					printf("Numero maximo de alunos cadastrado\n");
+					break;
+				}
+				cadastrar(aluno, idPont);
+				idPont++;
+				break;
+			case '2':
+				break;
+			case '3':
+				break;
+			case '4':
+				break;
+			case '5':
+				break;
+			case '6':
+				break;
+		}
+	}while(escolha != '6');
 
 	return 0;
 }
 
-void cadastro(est A[], int pont){
+char menu(){
+	char esc;
+
+	do{
+		printf("Cadastro de Alunos\n");
+		printf("1- Cadastrar\n");
+		printf("2- Listar\n");
+		printf("3- Pesquisar\n");
+		printf("4- Atualizar\n");
+		printf("5- Remover\n");
+		printf("6- Sair\n");
+
+		scanf(" %c", &esc);
+		if(esc != '1' && esc != '2' && esc != '3' && esc != '4' && esc != '5' && esc != '6'){
+			printf("comando invalido!\n");
+		}
+	}while(esc != '1' && esc != '2' && esc != '3' && esc != '4' && esc != '5' && esc != '6');
+
+	return esc;
+}
+
+void cadastrar(est A[], int pont){
 	/*
 	Pedir pelo:
 	Nome
 	Data de nascimento
 	CPF
 	Deixar situacao do cadastro como valido (=1)
-	Caso pont = MAX informar usuario
-	e nao chamar essa funcao
+	(feito) Caso pont == MAX informar usuario e nao chamar essa funcao
 	*/
+
+	//char de verificacao
+	char veri;
+	A[pont].id = pont;
+	do{
+		printf("Nome do aluno: ");
+		fgets(A[pont].nome, NOME-1, stdin);
+		veri = nomeVer(A[pont].nome);
+		if (veri == '0'){
+			printf("Nome invalido! (verificar se nao foi digitado algum numero ou caracter especial)\n");
+		}
+	}while(veri == '0');
+
+	puts(A[pont].nome);
+
 	return;
 }
 
@@ -102,4 +169,35 @@ void remover(est A[]){
 	situacao de cadastro (=0)
 	*/
 	return;
+}
+
+void sair(){
+	/*
+	escrever sair bonitinho
+	*/
+	return;
+}
+
+char nomeVer(char* nome[]){
+	/*
+	verificar se o nome contem apenas letras 
+	e nenhum caracter especial
+	*/
+
+	int i;
+	int tam = strlen(nome);
+
+
+	for(i = 0; i < tam; i++){
+		if(nome[i] == 'a' || nome[i] == 'b' || nome[i] == 'c' || nome[i] == 'd' || nome[i] == 'e' || nome[i] == 'f' || nome[i] == 'g' || nome[i] == 'h' || nome[i] == 'i' || nome[i] == 'j' || nome[i] == 'k' || nome[i] == 'l' || nome[i] == 'm' || nome[i] == 'n' || nome[i] == 'o' || nome[i] == 'p' || nome[i] == 'q' || nome[i] == 'r' || nome[i] == 's' || nome[i] == 't' || nome[i] == 'u' || nome[i] == 'v' || nome[i] == 'w' || nome[i] == 'x' || nome[i] == 'y' || nome[i] == 'z' || nome[i] == ' '){
+			if (i < tam - 1) continue;
+			else return '1';
+		}
+		else if(nome[i] == 'A' || nome[i] == 'B' || nome[i] == 'C' || nome[i] == 'D' || nome[i] == 'E' || nome[i] == 'F' || nome[i] == 'G' || nome[i] == 'H' || nome[i] == 'I' || nome[i] == 'J' || nome[i] == 'K' || nome[i] == 'L' || nome[i] == 'M' || nome[i] == 'N' || nome[i] == 'O' || nome[i] == 'P' || nome[i] == 'Q' || nome[i] == 'R' || nome[i] == 'S' || nome[i] == 'T' || nome[i] == 'U' || nome[i] == 'V' || nome[i] == 'W' || nome[i] == 'X' || nome[i] == 'Y' || nome[i] == 'Z'){
+			if (i < tam - 1) continue;
+			else return '1';
+		}
+		else return '0';
+	}
+
 }
